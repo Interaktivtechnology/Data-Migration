fbe6dd8ef581eff86aae22fcd4ecd0f9382968dc
veena <veena.upadhya@gmail.com>
2016-06-17 10:07:27 +0530

    Save Page

---
+++ src/react-view/component/migration-save.js
@@ -0,0 +1,134 @@
+'use strict';
+
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
+import {Router, browserHistory, withRouter} from 'react-router'
+import faker from 'faker'
+import moment from 'moment'
+
+import meta from './sample-meta'
+import meta2 from './sample-meta2'
+
+class MigrationConfig extends React.Component {
+
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      cols : this.props,
+      ds1 : localStorage.getItem('ds1'),
+      ds2 : localStorage.getItem('ds2'),
+      obj1 : localStorage.getItem('obj1'),
+      obj2 : localStorage.getItem('obj2'),
+      fil1 : localStorage.getItem('fil1'),
+      fil2 : localStorage.getItem('fil2'),
+      sconf : localStorage.getObj('sconf'),
+      dsel : localStorage.getObj('dsel'),
+      dconf : localStorage.getObj('dconf'),
+      fil : localStorage.getObj('fil')
+    }
+    }
+  componentDidMount(){
+    this.setState({
+      cols : this.props.cols
+    })
+  }
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      cols : nextProps.cols
+    })
+  }
+
+  render () {
+  return (
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered">
+          <thead>
+          <tr>
+            <th>DataSource 1</th>
+            <th>DataSource 2</th>
+            <th>Transformation / Formula</th>
+          </tr>
+          <tr>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+          <td key={100}>{this.state.ds1}</td>
+          <td key={101}>{this.state.ds2}</td>
+          <td key={102}></td>
+          </tr>
+          <tr>
+          <td key={103}>{this.state.obj1}</td>
+          <td key={104}>{this.state.obj2}</td>
+          <td key={105}></td>
+          </tr>
+          <tr>
+          <td key={106}>{this.state.fil1}</td>
+          <td key={107}>{this.state.fil2}</td>
+          <td key={108}></td>
+          </tr>
+          {
+            this.state.sconf.map((object, key) => {
+              return <tr>
+              <td key={110}>{object.Name}:{object.Value}</td>
+              <td key={111}>{this.state.dsel[object.Name]}:{this.state.dconf[object.Name]}</td>
+              <td key={112}>{this.state.fil[object.Name]}</td>
+              </tr>
+            })
+          }
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+      </div>
+    )
+  }
+}
+
+
+class MigrationSave extends React.Component {
+
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      jombotron : {display: 'block'},
+      cols : 2,
+    }
+    this._remCols = this._remCols.bind(this)
+    this._addCols = this._addCols.bind(this)
+  }
+  componentWillMount(){
+  }
+  componentDidMount(){
+  }
+
+  _addCols () {
+    this.state.cols += 1
+    this.setState({
+      cols : this.state.cols
+    })
+  }
+  _remCols(){
+    this.state.cols -= 1
+    this.setState({
+      cols : this.state.cols
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="col-md-12">
+        <h1>Object Field Merging</h1>
+        <MigrationConfig router={this.props.router} cols={this.state.cols}></MigrationConfig>
+		</div>
+        );
+  }
+}
+MigrationSave.contextType = {
+  router : React.PropTypes.func.isRequired
+}
+export {MigrationConfig}
+export default withRouter(MigrationSave)
--- src/react-view/routes/index.js
+++ src/react-view/routes/index.js
@@ -5,6 +5,7 @@ var {DefaultRoute, NotFoundRoute, Route} = require('react-router')
 import NavbarInstance from '../common/navbar'
 import DsConfig from '../component/ds-config'
 import Migration from '../component/migration'
+import MigrationSave from '../component/migration-save'
 import MigrationList from '../component/migration-list'
 import MigrationRow from '../component/migration-row'
 import MigrationRowDetail from '../component/migration-row-detail'
@@ -20,6 +21,7 @@ const rootRoute = <Route path='/' component={NavbarInstance} title={'Home'}>
   <Route path='/ds-config' title="Data Source Configuration" component={DsConfig}></Route>
   <Route path='/migration' title="Migration List" component={MigrationList}>
     <Route path='/migration/new' title="New Migration" component={Migration}></Route>
+    <Route path='/migration/save' title="Save Migration" component={MigrationSave}></Route>      
     <Route path='/migration/edit' title="Edit Migration" component={Migration}></Route>
     <Route path='/migration/view/:id' title="Migration Detail" component={MigrationRow} />
     <Route path='/migration/fix-conflict/:id' title="Fix Conflict" component={MigrationRow}>
